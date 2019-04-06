7476e18bb645d4ae8df36ff2f66477e6ac4d6d9f
Stefan Alpers <stefan.alpers@gmx.net>
2019-04-06 18:09:13 +0200

    Interface IBoundedQueue

---
+++ BoundedQueue/Components.BoundedQueue/Interfaces/IBoundedQueue.cs
@@ -0,0 +1,13 @@
+﻿namespace Components.BoundedQueue.Interfaces
+{
+    public interface IBoundedQueue<T>
+    {
+        void Enqueue(T Element);
+
+        T Dequeue();
+
+        int Count();
+
+        int Size();
+    }
+}
